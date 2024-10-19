95988cdf5e623e4a3edfd8519e37c85c0a216910
titus <tituskennedy74@gmail.com>
2024-10-19 14:58:19 +0300

    just code

--- app.py
+++ app.py
@@ -50,8 +50,3 @@ def create_app():
     app.register_blueprint(messaging)
 
     return app
-    
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
