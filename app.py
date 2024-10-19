c7fff074f280c60282545153cc4d9100a7705f7b
titus <tituskennedy74@gmail.com>
2024-10-19 14:59:47 +0300

    just code

--- app.py
+++ app.py
@@ -20,33 +20,29 @@ login_manager = LoginManager()
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Function to create Flask app
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    # Initialize Flask extensions
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'main.login'
-    login_manager.login_message = "Login to continue"
-    
-    app.config['REMEMBER_COOKIE_DURATION'] = timedelta(minutes=30)
+# Initialize Flask extensions
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'main.login'
+login_manager.login_message = "Login to continue"
 
-    # Create database tables
-    with app.app_context():
-        db.create_all()
+app.config['REMEMBER_COOKIE_DURATION'] = timedelta(minutes=30)
 
-    # Set up Flask secret key
-    foo = secrets.token_urlsafe(16)
-    app.secret_key = foo
+# Create database tables
+with app.app_context():
+    db.create_all()
 
-    # Register Flask blueprints
-    bootstrap = Bootstrap5(app)
-    csrf = CSRFProtect()
-    app.register_blueprint(main)
-    app.register_blueprint(master)
-    app.register_blueprint(follow)
-    app.register_blueprint(messaging)
+# Set up Flask secret key
+foo = secrets.token_urlsafe(16)
+app.secret_key = foo
 
-    return app
+# Register Flask blueprints
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect()
+app.register_blueprint(main)
+app.register_blueprint(master)
+app.register_blueprint(follow)
+app.register_blueprint(messaging)
